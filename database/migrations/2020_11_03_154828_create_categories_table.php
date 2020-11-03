5ba9bf230ce511f4d7ce84219aa024dafa3f2cac
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-11-03 13:49:27 -0300

    Criação da migration de categorias

--- database/migrations/2020_11_03_154828_create_categories_table.php
+++ database/migrations/2020_11_03_154828_create_categories_table.php
@@ -15,6 +15,11 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('slug');
+            
             $table->timestamps();
         });
     }
