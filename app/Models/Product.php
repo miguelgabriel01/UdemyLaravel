311ca5024595c3c439a112e7dd3aa762b2b228f2
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-11-03 16:09:17 -0300

    Criação do factory de store,category e produtcs

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,8 @@ class Product extends Model
     public function store(){
         return $this->belongsTo(Store::class);
     }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
