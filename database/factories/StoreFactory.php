a67a6f3aaf41d2166647519925fd0ccb0bd93dd5
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-11-11 23:15:10 -0300

    Criação do controller de store

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -1,28 +1,12 @@
-<?php
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
-namespace Database\Factories;
-
-use App\Models\Store;
-use Illuminate\Database\Eloquent\Factories\Factory;
-
-class StoreFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Store::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            //
-        ];
-    }
-}
+$factory->define(App\User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+    ];
+});
